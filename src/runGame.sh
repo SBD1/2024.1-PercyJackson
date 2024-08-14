283212d128e2167fb0b4b9ab0baf56b49ad6a80a
Natan Santana <natan.santana.dev@gmail.com>
2024-08-14 18:34:36 -0300

    Adição do python e da configuração de conexão com banco

--- src/runGame.sh
+++ src/runGame.sh
@@ -1,3 +1,9 @@
 sudo docker compose down
 sudo docker volume rm src_db_data
-sudo docker compose up --build
+sudo docker compose up --build -d # Executa em modo detached (em segundo plano)
+
+# Aguarda alguns segundos para garantir que o banco de dados esteja pronto
+sleep 10
+
+# Executa um terminal interativo no container do serviço `api`
+sudo docker exec -it $(sudo docker ps -qf "name=api") /bin/bash -c "python3 main.py"
