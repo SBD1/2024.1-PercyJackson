01aa8b4f5125dd6c554f0e94c164de9b8be05bd6
owhenrique <me.pauloalmeida@gmail.com>
2024-08-19 12:43:41 -0300

    fix: areaTeletransporte varchar length

--- src/init.sql
+++ src/init.sql
@@ -6,8 +6,9 @@ CREATE DOMAIN IntPositivo AS INTEGER
 
 CREATE TABLE constante
 (
-    nome VARCHAR(20),
-    valor IntPositivo NOT NULL,
+    nome    VARCHAR(20),
+    valor   IntPositivo NOT NULL,
+    
     PRIMARY KEY(nome)
 );
 
@@ -47,18 +48,20 @@ CREATE TABLE area
 
 CREATE TABLE nivel
 (
-    id INTEGER,
-    pontosExperiencia IntPositivo NOT NULL,
+    id                  INTEGER,
+    pontosExperiencia   IntPositivo NOT NULL,
+    
     PRIMARY KEY(id)
 );
 
 CREATE TABLE deus
 (
     nome VARCHAR(15),
-    forcaInicial AtributoInicial NOT NULL,
-    intelectoInicial AtributoInicial NOT NULL,
-    agilidadeInicial AtributoInicial NOT NULL,
-    combateInicial AtributoInicial NOT NULL,
+    forcaInicial        AtributoInicial NOT NULL,
+    intelectoInicial    AtributoInicial NOT NULL,
+    agilidadeInicial    AtributoInicial NOT NULL,
+    combateInicial      AtributoInicial NOT NULL,
+    
     PRIMARY KEY(nome)
 );
 
@@ -66,19 +69,21 @@ CREATE TABLE tipoPersonagem
 (
     nome VARCHAR(15),
     tipo CHAR NOT NULL CHECK(tipo = 'J' OR tipo = 'I'),
+    
     PRIMARY KEY(nome)
 );
 
 CREATE TABLE inimigo
 (
-    nome VARCHAR(15),
-    vidaMax IntPositivo NOT NULL,
-    forca IntPositivo NOT NULL,
-    intelecto IntPositivo NOT NULL,
-    agilidade IntPositivo NOT NULL,
-    combate IntPositivo NOT NULL,
-    pontosExperiencia IntPositivo NOT NULL,
-    nivel INTEGER NOT NULL,
+    nome                VARCHAR(15),
+    vidaMax             IntPositivo NOT NULL,
+    forca               IntPositivo NOT NULL,
+    intelecto           IntPositivo NOT NULL,
+    agilidade           IntPositivo NOT NULL,
+    combate             IntPositivo NOT NULL,
+    pontosExperiencia   IntPositivo NOT NULL,
+    nivel               INTEGER NOT NULL,
+    
     PRIMARY KEY(nome),
     FOREIGN KEY(nome) REFERENCES tipoPersonagem(nome) ON DELETE RESTRICT,
     FOREIGN KEY(nivel) REFERENCES nivel(id) ON DELETE RESTRICT
@@ -86,20 +91,21 @@ CREATE TABLE inimigo
 
 CREATE TABLE jogador
 (
-    nome VARCHAR(15),
-    vidaMax IntPositivo NOT NULL DEFAULT 50,
-    forca IntPositivo NOT NULL,
-    intelecto IntPositivo NOT NULL,
-    agilidade IntPositivo NOT NULL,
-    combate IntPositivo NOT NULL,
-    vidaAtual IntPositivo NOT NULL DEFAULT 50 CHECK(vidaAtual <= vidaMax),
-    experienciaAtual IntPositivo NOT NULL DEFAULT 0,
-    armadura INTEGER,
-    arma INTEGER,
-    itemMagico INTEGER,
-    deus VARCHAR(15) NOT NULL, 
-    areaAtual VARCHAR(30) NOT NULL,
-    nivel INTEGER NOT NULL DEFAULT 1,
+    nome                VARCHAR(15),
+    vidaMax             IntPositivo NOT NULL DEFAULT 50,
+    forca               IntPositivo NOT NULL,
+    intelecto           IntPositivo NOT NULL,
+    agilidade           IntPositivo NOT NULL,
+    combate             IntPositivo NOT NULL,
+    vidaAtual           IntPositivo NOT NULL DEFAULT 50 CHECK(vidaAtual <= vidaMax),
+    experienciaAtual    IntPositivo NOT NULL DEFAULT 0,
+    armadura            INTEGER,
+    arma                INTEGER,
+    itemMagico          INTEGER,
+    deus                VARCHAR(15) NOT NULL, 
+    areaAtual           VARCHAR(30) NOT NULL,
+    nivel               INTEGER NOT NULL DEFAULT 1,
+    
     PRIMARY KEY(nome),
     FOREIGN KEY(nome) REFERENCES tipoPersonagem(nome) ON DELETE RESTRICT,
     FOREIGN KEY(deus) REFERENCES deus(nome) ON DELETE RESTRICT,
@@ -118,11 +124,12 @@ CREATE TABLE jogador
 
 CREATE TABLE inimigoConcreto
 (
-    nomeConcreto VARCHAR(15),
-    vidaAtual IntPositivo NOT NULL, /* Criar Trigger para definir a vidaAtual como a vidaMax do inimigo de referência */
-    inimigo VARCHAR(15) NOT NULL,
-    areaAtual VARCHAR(30),
-    loot VARCHAR(25) NOT NULL,
+    nomeConcreto    VARCHAR(15),
+    vidaAtual       IntPositivo NOT NULL, /* Criar Trigger para definir a vidaAtual como a vidaMax do inimigo de referência */
+    inimigo         VARCHAR(15) NOT NULL,
+    areaAtual       VARCHAR(30),
+    loot            VARCHAR(25) NOT NULL,
+    
     PRIMARY KEY(nomeConcreto),
     FOREIGN KEY(areaAtual) REFERENCES area(nome) ON DELETE RESTRICT,
     FOREIGN KEY(inimigo) REFERENCES inimigo(nome) ON DELETE RESTRICT
@@ -138,7 +145,8 @@ CREATE TABLE abate
     nomeJogador VARCHAR(15),
     nomeInimigo VARCHAR(15),
     dataHorario TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    resultado BOOLEAN NOT NULL,
+    resultado   BOOLEAN NOT NULL,
+    
     PRIMARY KEY(nomeJogador, nomeInimigo, dataHorario),
     FOREIGN KEY(nomeJogador) REFERENCES jogador(nome) ON DELETE RESTRICT,
     FOREIGN KEY(nomeInimigo) REFERENCES inimigo(nome) ON DELETE RESTRICT
@@ -146,23 +154,26 @@ CREATE TABLE abate
 
 CREATE TABLE inventario
 (
-    jogador VARCHAR(15),
+    jogador     VARCHAR(15),
     cargaMaxima INTEGER NOT NULL CHECK (cargaMaxima >= 5),
+    
     PRIMARY KEY(jogador),
     FOREIGN KEY(jogador) REFERENCES jogador(nome) ON DELETE CASCADE
 );
 
 CREATE TABLE tipoItem
 (
-    nome VARCHAR(30),
+    nome          VARCHAR(30),
     classificacao VARCHAR(15) NOT NULL CHECK (classificacao IN ('Defesa', 'Ataque', 'Consumivel', 'Magico')),
+    
     PRIMARY KEY(nome)
 );
 
 CREATE TABLE itemInventario
 (
     jogador VARCHAR(15),
-    item VARCHAR(30),
+    item    VARCHAR(30),
+    
     PRIMARY KEY(jogador, item),
     FOREIGN KEY(jogador) REFERENCES jogador(nome) ON DELETE CASCADE,
     FOREIGN KEY(item) REFERENCES tipoItem(nome) ON DELETE CASCADE
@@ -170,11 +181,12 @@ CREATE TABLE itemInventario
 
 CREATE TABLE defesa
 (
-    nome VARCHAR(30),
-    areaAtual VARCHAR(30),
-    descricao TEXT,
-    peso IntPositivo,
-    modDefesa IntPositivo,
+    nome        VARCHAR(30),
+    areaAtual   VARCHAR(30),
+    descricao   TEXT,
+    peso        IntPositivo,
+    modDefesa   IntPositivo,
+    
     PRIMARY KEY(nome), 
     FOREIGN KEY(nome) REFERENCES tipoItem(nome) ON DELETE CASCADE
     -- FOREIGN KEY(areaAtual) REFERENCES area(nome) ON DELETE RESTRICT,
@@ -182,12 +194,13 @@ CREATE TABLE defesa
 
 CREATE TABLE ataque
 (
-    nome VARCHAR(30),
-    areaAtual VARCHAR(30),
-    descricao TEXT,
-    peso IntPositivo,
-    modCombate IntPositivo,
-    modForca IntPositivo,
+    nome        VARCHAR(30),
+    areaAtual   VARCHAR(30),
+    descricao   TEXT,
+    peso        IntPositivo,
+    modCombate  IntPositivo,
+    modForca    IntPositivo,
+    
     PRIMARY KEY(nome), 
     FOREIGN KEY(nome) REFERENCES tipoItem(nome) ON DELETE CASCADE
     -- FOREIGN KEY(areaAtual) REFERENCES area(nome) ON DELETE RESTRICT,
@@ -195,17 +208,18 @@ CREATE TABLE ataque
 
 CREATE TABLE magico
 (
-    nome VARCHAR(30),
-    areaAtual VARCHAR(30),
-    descricao TEXT,
-    peso IntPositivo,
-    modCombate IntPositivo,
-    modForca IntPositivo,
-    modDefesa IntPositivo,
-    modAgilidade IntPositivo,
-    modCarga IntPositivo,
-    tempoDeRecarga IntPositivo,
-    tempoAtual IntPositivo,
+    nome            VARCHAR(30),
+    areaAtual       VARCHAR(30),
+    descricao       TEXT,
+    peso            IntPositivo,
+    modCombate      IntPositivo,
+    modForca        IntPositivo,
+    modDefesa       IntPositivo,
+    modAgilidade    IntPositivo,
+    modCarga        IntPositivo,
+    tempoDeRecarga  IntPositivo,
+    tempoAtual      IntPositivo,
+
     PRIMARY KEY(nome), 
     FOREIGN KEY(nome) REFERENCES tipoItem(nome) ON DELETE CASCADE
     -- FOREIGN KEY(areaAtual) REFERENCES area(nome) ON DELETE RESTRICT,
@@ -213,12 +227,13 @@ CREATE TABLE magico
 
 CREATE TABLE consumivel
 (
-    nome VARCHAR(30),
-    areaAtual VARCHAR(30),
-    descricao TEXT,
-    peso IntPositivo,
-    vidaRecuperada IntPositivo,
-    areaTeletransporte VARCHAR(15),
+    nome                VARCHAR(30),
+    areaAtual           VARCHAR(30),
+    descricao           TEXT,
+    peso                IntPositivo,
+    vidaRecuperada      IntPositivo,
+    areaTeletransporte  VARCHAR(30),
+
     PRIMARY KEY(nome), 
     FOREIGN KEY(nome) REFERENCES tipoItem(nome) ON DELETE CASCADE
     -- FOREIGN KEY(areaAtual) REFERENCES area(nome) ON DELETE RESTRICT,
@@ -232,7 +247,7 @@ CREATE TABLE armadilha
     DTForca             INTEGER     NOT NULL,
     DTAgilidade         INTEGER     NOT NULL,
     DTInteligencia      INTEGER     NOT NULL,
-    areaTeletransporte  VARCHAR(15) NOT NULL,
+    areaTeletransporte  VARCHAR(30) NOT NULL,
 
     PRIMARY KEY(id),
     FOREIGN KEY(id)                 REFERENCES desafio(id)          ON DELETE RESTRICT,
